c474e572811d20e79f32e185335ffb0101299bc0
Shivang Gupta <shivanggupta2468@gmail.com>
2025-06-21 03:51:45 +0530

    Update Schemas.sql

--- Schemas.sql
+++ Schemas.sql
@@ -1,1 +1,35 @@
+-- SCHEMAS OF ONLINE BOOK STORE
 
+Drop Table if exists customers;
+Create table customers (
+  customer_id int primary key,
+  name varchar(100),
+  email varchar(100),
+  phone varchar(50),
+  city varchar(150),
+  country varchar(150)
+);
+
+Drop table if exists books;
+create table books(
+  book_id int primary key,
+  title varchar(100),
+  author varchar(100),
+  genre varchar(50),
+  published_year int ,
+  price decimal(8,2),
+  stock int
+);
+
+drop table if exists orders;
+create table orders(
+  order id int primary key,
+  customer_id int ,
+  book_id int ,
+  order_date date,
+  quantity int ,
+  total amount decimal(8,2),
+  foreign key (customer_id) references customers(customer_id),
+  foreign key (book_id) references books(book_id)
+    
+);
