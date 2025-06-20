0aadf70fc28d01df6849c19950d3e8712fa15279
Shivang Gupta <shivanggupta2468@gmail.com>
2025-06-21 03:35:08 +0530

    Update Business_Problems.sql

--- Business_Problems.sql
+++ Business_Problems.sql
@@ -1,1 +1,18 @@
+-- 15 Business Problems & Solutions
+
+1. List all customers from the Canada 
+2. Find the book with the Lowest Stock 
+3. Calculate the total revenue generated from all orders
+4. Retrieve all orders where the total amount exceeds $20
+5. Show all customers who ordered more than 1 quantity of a book
+6. Show orders placed in November 2023
+7. Retrieve the total number of books sold for each genre
+8. Find the average price of books in the "Fantasy" genre
+9. List customers who have placed at least 2 orders
+10.Find the most frequently ordered book
+11.Show the top 3 most expensive books of "Fantasy" genre
+12.Retrieve the total quantity of books sold by each author 
+13.List the cities where Customers who spend over $30 are located
+14.Find the customer who spend the most on orders
+15.Calculate the stock remaining after fulfilling all orders
 
